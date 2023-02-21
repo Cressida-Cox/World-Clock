782aec54050cf40f9052222d6f5416c1b69e2e77
Cressida Cox <cox.cressie@yahoo.co.uk>
2023-02-21 19:17:16 +0000

    selected location time update

--- Javascript/index.js
+++ Javascript/index.js
@@ -47,9 +47,12 @@ function updateCity(event) {
   }
 
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
+
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+
+  function updateCityTime() {
+    let cityTime = moment().tz(cityTimeZone);
+    citiesElement.innerHTML = ` 
     <div class="city">
         <div>
           <h2>${cityName}</h2>
@@ -61,6 +64,10 @@ function updateCity(event) {
       </div>
   <a href="/">All cities</a>
   `;
+  }
+
+  updateCityTime();
+  setInterval(updateCityTime, 1000);
 }
 
 updateTime();
